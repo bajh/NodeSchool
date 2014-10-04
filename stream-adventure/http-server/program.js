2ebb9cc7bb8af25ccd03ebaec41a46bf08ca6694
Brandon Hopkins <brhpkns@gmail.com>
2014-10-04 11:48:15 -0400

    HTTP server

---
+++ stream-adventure/http-server/program.js
@@ -0,0 +1,15 @@
+var http = require('http'),
+    through = require('through');
+
+var server = http.createServer(function(req, res){
+  if (req.method == 'POST') {
+    req.pipe(through(write)).pipe(res);
+  }
+
+  function write(buf){
+    this.queue(buf.toString().toUpperCase());
+  }
+
+})
+
+server.listen(process.argv[2]);
